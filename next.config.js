0b44cea5b9743f0cc04843b6b421209972214a97
JosephASG <alexis200123@outlook.es>
2024-11-03 21:13:45 -0500

    first commit Qubex Docs

---
+++ next.config.js
@@ -0,0 +1,24 @@
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.tsx',
+})
+
+module.exports = withNextra({
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+  async redirects() {
+    return [
+      {
+        source: '/anythingllm-cloud/502',
+        destination: '/cloud/error-502',
+        permanent: true,
+      },
+    ];
+  },
+});
